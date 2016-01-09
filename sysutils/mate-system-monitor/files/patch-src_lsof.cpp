cdb07d8a901ad51c87c89fd671d8e1d4b019ab52
Koop Mast <kwm@rainbow-runner.nl>
2016-01-09 12:25:56 +0100

    Tweak mate-system-monitor patch a bit to flush the ostringstream.

--- sysutils/mate-system-monitor/files/patch-src_lsof.cpp
+++ sysutils/mate-system-monitor/files/patch-src_lsof.cpp
@@ -1,11 +1,11 @@
---- src/lsof.cpp.orig	2016-01-09 12:10:26.760298000 +0100
-+++ src/lsof.cpp	2016-01-09 12:10:37.441213000 +0100
+--- src/lsof.cpp.orig	2015-09-25 13:36:19.000000000 +0200
++++ src/lsof.cpp	2016-01-09 12:24:55.546084000 +0100
 @@ -140,7 +140,7 @@
  
          void update_count(unsigned count)
          {
 -            string s = static_cast<std::ostringstream&>(std::ostringstream() << count).str();
-+            string s = (std::ostringstream() << count).str();
++            string s = static_cast<std::ostringstream&>(std::ostringstream().flush() << count).str();
              gtk_label_set_text(this->count, s.c_str());
          }
  
