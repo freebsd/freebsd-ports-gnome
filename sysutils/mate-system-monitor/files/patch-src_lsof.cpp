eed9de1d56c3f4499ecacc5c10a7feff0663301f
Koop Mast <kwm@rainbow-runner.nl>
2016-01-09 19:07:21 +0100

    even more simplification.

--- sysutils/mate-system-monitor/files/patch-src_lsof.cpp
+++ sysutils/mate-system-monitor/files/patch-src_lsof.cpp
@@ -1,11 +1,14 @@
 --- src/lsof.cpp.orig	2015-09-25 13:36:19.000000000 +0200
-+++ src/lsof.cpp	2016-01-09 12:24:55.546084000 +0100
-@@ -140,7 +140,7 @@
++++ src/lsof.cpp	2016-01-09 12:32:53.892273000 +0100
+@@ -140,8 +140,9 @@
  
          void update_count(unsigned count)
          {
 -            string s = static_cast<std::ostringstream&>(std::ostringstream() << count).str();
-+            string s = static_cast<std::ostringstream&>(std::ostringstream().flush() << count).str();
-             gtk_label_set_text(this->count, s.c_str());
+-            gtk_label_set_text(this->count, s.c_str());
++	    std::ostringstream ss;
++	    ss << count;;
++            gtk_label_set_text(this->count, ss.str().c_str());
          }
+ 
  
