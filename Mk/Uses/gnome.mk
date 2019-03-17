767933b40fc17ec9ea8432ed17774ea8dcac7b6e
Koop Mast <kwm@rainbow-runner.nl>
2019-03-17 14:30:15 +0100

    Add cairomm 1.15.5, API 1.16

--- Mk/Uses/gnome.mk
+++ Mk/Uses/gnome.mk
@@ -95,7 +95,7 @@ _USE_GNOME_ALL+=amtk dconf evolutiondataserver3 gnomecontrolcenter3 gnomedesktop
 _USE_GNOME_ALL+=gtk40
 
 # C++ bindings
-_USE_GNOME_ALL+=atkmm cairomm gconfmm26 glibmm gtkmm20 gtkmm24 \
+_USE_GNOME_ALL+=atkmm cairomm cairomm116 gconfmm26 glibmm gtkmm20 gtkmm24 \
 		gtkmm30 gtksourceviewmm3 libgdamm5 \
 		libgtksourceviewmm libxml++26 libsigc++12 libsigc++20 \
 		libsigc++30 pangomm
@@ -133,6 +133,8 @@ cairo_LIB_DEPENDS=	libcairo.so:graphics/cairo
 
 cairomm_LIB_DEPENDS=	libcairomm-1.0.so:graphics/cairomm
 cairomm_USE_GNOME_IMPL=	cairo libxml++26
+
+cairomm116_LIB_DEPENDS=	libcairomm-1.16.so:graphics/cairomm116
 
 gconfmm26_LIB_DEPENDS=		libgconfmm-2.6.so:devel/gconfmm26
 gconfmm26_USE_GNOME_IMPL=	glibmm gconf2
