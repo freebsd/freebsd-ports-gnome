a7c768e3e451637a14bbe534b3b2f009007c37c2
Koop Mast <kwm@rainbow-runner.nl>
2019-07-28 10:32:49 +0200

    Update gnome-builder to 3.32.4.

--- devel/gnome-builder/files/patch-src_libide_io_ide-gfile.c
+++ devel/gnome-builder/files/patch-src_libide_io_ide-gfile.c
@@ -1,6 +1,6 @@
---- src/libide/io/ide-gfile.c.orig	2019-02-19 02:44:09 UTC
+--- src/libide/io/ide-gfile.c.orig	2019-07-15 23:20:44 UTC
 +++ src/libide/io/ide-gfile.c
-@@ -366,7 +366,7 @@ ide_g_file_get_children_async (GFile               *fi
+@@ -367,7 +367,7 @@ ide_g_file_get_children_async (GFile               *fi
    ide_task_set_priority (task, io_priority);
    ide_task_set_task_data (task, gc, get_children_free);
  
