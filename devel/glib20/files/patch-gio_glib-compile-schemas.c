1a7c50c0f81af04600f3a93768210a8e40dab789
Charlie Li <git@vishwin.info>
2020-06-15 13:32:08 -0400

    devel/glib20: integrate changes from D25279

--- devel/glib20/files/patch-gio_glib-compile-schemas.c
+++ devel/glib20/files/patch-gio_glib-compile-schemas.c
@@ -1,6 +1,6 @@
---- gio/glib-compile-schemas.c.orig	2017-08-07 16:58:31.000000000 +0200
-+++ gio/glib-compile-schemas.c	2017-09-15 09:25:19.746310000 +0200
-@@ -1219,6 +1219,7 @@ parse_state_start_schema (ParseState  *state,
+--- gio/glib-compile-schemas.c.orig	2020-05-20 10:46:43 UTC
++++ gio/glib-compile-schemas.c
+@@ -1232,6 +1232,7 @@ parse_state_start_schema (ParseState  *state,
        return;
      }
  
@@ -8,7 +8,7 @@
    if (path && (g_str_has_prefix (path, "/apps/") ||
                 g_str_has_prefix (path, "/desktop/") ||
                 g_str_has_prefix (path, "/system/")))
-@@ -1231,6 +1232,7 @@ parse_state_start_schema (ParseState  *state,
+@@ -1244,6 +1245,7 @@ parse_state_start_schema (ParseState  *state,
        g_printerr ("%s\n", message);
        g_free (message);
      }
